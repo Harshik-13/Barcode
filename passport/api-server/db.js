5ee061cc2267a503de6c90ad591f12752812a183
Harshik-13 <jagirapuharshik13@gmail.com>
2026-07-26 17:27:07 +0530

    Remove stale design docs and legacy code

--- passport/api-server/db.js
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-
-const dbPath = process.env.DATABASE_URL || path.join(__dirname, 'campus_passport.db');
-const db = new Database(dbPath);
-
-module.exports = db;
